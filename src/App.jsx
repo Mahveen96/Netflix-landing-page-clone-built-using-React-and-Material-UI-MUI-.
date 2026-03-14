311a88968eb45a2a453978a4c673bedbb76e37ea
Mahveen <98329413+Mahveen96@users.noreply.github.com>
2026-03-13 23:24:05 -0500

    Clean up App component by removing unused code
    Removed unused imports and commented components from App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-// import { useState } from 'react'
 import Navbar from './Components/Navbar.jsx'
 import Hero from './Components/Hero.jsx'
-// import Banner from './Components/Banner.jsx'
-// import MovieRow from './Components/MovieRow.jsx'
-// import MovieCard from './Components/MovieCard.jsx'
+
 
 
 function App() {
@@ -11,9 +8,6 @@ function App() {
   <div>
     <Navbar />
     <Hero />
-    {/* <Banner /> */}
-    {/* <MovieCard /> */}
-    {/* <MovieRow /> */}
   </div>
  )
 }
